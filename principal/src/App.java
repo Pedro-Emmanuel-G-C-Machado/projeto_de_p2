5145ad11d9ee338b5f8c4690d1fd33cf05ef84ae
Pedro-Emmanuel-G-C-Machado <pedroemmanuel.g.c.machado@gmail.com>
2023-07-02 19:09:31 -0300

    criando classe capitulo

--- principal/src/App.java
+++ principal/src/App.java
@@ -20,7 +20,7 @@ public class App{
       player3.forces = "exercito";
      
 
-    // depois do if vamos fazer a segunda seleção. (FORCES = FORÇAS)
+    
     if(player1.apto()){
       Capitulos capitulo_0 = new Capitulos();
       System.out.println("----- Capitulo 0 -------");
@@ -35,6 +35,7 @@ public class App{
         capitulo_1.narrativa = "Bom, aqui o senhor(a): " + player1.name + ", vai para os testes de aptidao fisica tem que fazer cinco flexoes, tres barras e vinte abdominais. Voce quer fazer agora ou deixar para amanha ? O que voce quer, hoje ou amanha ?";
         capitulo_1.escolha1 = "hoje";
         capitulo_1.escolha2 = "amanha";
+        capitulo_1.mostrar(player1);
         if(capitulo_1.escolha(texto) == 1){
           System.out.println("Voce estava preparado para fazer todos os exercicios e foi para o escritorio assinar o contrato para ser encaminhado para o treinamento. Parabens," + player1.name + " ,voce esta no airborn, cia 22°, bem vindo ao exercito.");
         }
@@ -42,12 +43,14 @@ public class App{
           System.out.println("Voce estava descansado, mas não conseguiu fazer os exercicios, você vai ter que ir embora filho.");
           player1.treinoNow = false;
         }
+        
       }
       else{
         Capitulos capitulo_2 = new Capitulos();
         capitulo_2.narrativa = "Certo, voce tem que fazer teste de apitidao fisica, sao dois quilometros de corrida, tres quilometros e meio de natcao no mar, tres barras e quarenta abdominais. Voce quer fazer  hoje ou  amanha ?";
         capitulo_2.escolha1 = "hoje";
-        capitulo_2.escolha1 = "amanha";
+        capitulo_2.escolha2 = "amanha";
+        capitulo_2.mostrar(player1);
         if(capitulo_2.escolha(texto)== 1){
           System.out.println("Você estava enfadado de mais para conseguir nadar e correr tanto e foi reprovado, pode ir embora garoto. ");
           player1.treinoNow = false;
